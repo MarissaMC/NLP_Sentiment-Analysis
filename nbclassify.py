ae394940245085141aab7c5aa679b586caaa8d5e
Marissa Wu <mengchun@usc.edu>
2015-02-18 06:44:54 +0000

    1 change the judge code according to the model changed in nblearn.py

--- nbclassify.py
+++ nbclassify.py
@@ -1,61 +1,48 @@
 import sys
 import re, string
 import math
+import operator
 
 model = sys.argv[1]
 testfile = sys.argv[2]
+#output=sys.argv[3]
 
 from collections import defaultdict
 import json
 
 dic = json.load(open(model))
 
-# get the prob of class, conditional prob
 
-P_ham=dic['pro_ham']
-P_spam=dic['pro_spam']
+# P_wc
+length=len(dic['N_rank'])
+dic['Num_Work_class']=[0]*length
 
-wc_ham=dic['num_ham']
-wc_spam=dic['num_spam']
+for c in dic['N_rank']:
 
+    r_c=dic['N_rank'][c]
+    for w in dic:
+        if w!='N_rank' and w!='Num_class' and w!='Num_Work_class':
+           dic['Num_Work_class'][r_c]+=dic[w][r_c]
 
-if model=='spam.nb':
-    switch=0
-if model=='sentiment.nb':
-    switch=1
-   
-if switch==0:
-    a="HAM"
-    b="SPAM"
-if switch==1:
-    a="POS"
-    b="NEG"
-
-x=0
-
-pun = set(string.punctuation)
+    for w in dic:
+        if w!='N_rank' and w!='Num_class' and w!='Num_Work_class':
+           dic[w][r_c]=dic[w][r_c]/float(dic['Num_Work_class'][r_c])  
 
 
-with open(testfile,'r') as f:
+#with open(output,'w') as outp:
+with open(testfile,'r',errors='ignore') as f:
      for line in f:
-         ham_cp=1
-         spam_cp=1
 
-         for word in line.split():
-             if word not in pun:
-        	     ham_word='h_'+word
-        	     spam_word='s_'+word
-        	
-             if ham_word in dic:
-                ham_cp+=math.log(float(dic[ham_word])/float(wc_ham))        
-                if spam_word in dic:
-                      spam_cp+=math.log(float(dic[spam_word])/float(wc_spam))
-            
-         #classify_ham=float(P_ham*ham_cp)/float(P_ham*ham_cp+P_spam*spam_cp)
-         
-         if math.log(1)+math.log(P_ham)+ham_cp > math.log(P_spam)+spam_cp:
-            print(a)
-         else:
-            print(b)
+         judge=defaultdict()
+
+         for c in dic['N_rank']:
+             r_c=dic['N_rank'][c]
+             judge[c]=math.log(dic['Num_class'][r_c])
+             for word in line.split():
+                 if word in dic:
+                    judge[c]+=math.log(dic[word][r_c])
+          
+         sys.stdout.write(max(judge, key=judge.get))
+         sys.stdout.write('\n')
  
 
